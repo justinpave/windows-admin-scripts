6e0a2f349121b6cda8589f28dac39c6c32097999
Justin Pave <justin.pave@gmail.com>
2025-08-15 09:26:39 -0600

    Create Invoke-WindowsUpdate.ps1

---
+++ scripts/Maintenance/Invoke-WindowsUpdate.ps1
@@ -0,0 +1,12 @@
+windows-admin-scripts/
+├─ scripts/
+│  ├─ Inventory/
+│  │  └─ Get-SystemInventory.ps1
+│  ├─ Maintenance/
+│  │  └─ Invoke-WindowsUpdate.ps1
+│  ├─ Monitoring/
+│  │  └─ Collect-Logs.ps1
+│  └─ Security/
+│     └─ Export-FirewallRules.ps1
+├─ outputs/    (this will be created when scripts run, add to .gitignore)
+└─ README.md
